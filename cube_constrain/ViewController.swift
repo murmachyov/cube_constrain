a75ebb6d092b3484a4b5d9a54982e17cd257062e
Dmitry Murmachyov <murmachyov@gmail.com>
2022-02-15 09:32:51 +0300

    Initial Commit

---
+++ cube_constrain/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cube_constrain
+//
+//  Created by Murmachyov on 15.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
